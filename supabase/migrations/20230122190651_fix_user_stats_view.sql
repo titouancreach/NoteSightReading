33116f9b5a39eb1e755fdb12f7590dcdd3ceac71
Titouan CREACH <t.creach@unitee.io>
2023-01-22 20:10:32 +0100

    fix the user_stats view

---
+++ supabase/migrations/20230122190651_fix_user_stats_view.sql
@@ -0,0 +1,32 @@
+
+CREATE OR REPLACE VIEW user_stats AS 
+
+SELECT security.user_id
+    , security.best_attempt
+    , security.rank
+    , security.attempts_count
+FROM (
+    SELECT gs.user_id
+        , max(q.total) AS best_attempt
+        , rank() OVER (
+            ORDER BY (sum(q.total)) DESC
+            ) AS rank
+        , count(q.total) AS attempts_count
+    FROM game_sessions gs
+    JOIN (
+        SELECT attempts.game_session_id
+            , sum(CASE attempts.is_success
+                    WHEN true
+                        THEN 15
+                    ELSE '-10'::INTEGER
+                    END) AS total
+        FROM attempts
+        GROUP BY attempts.game_session_id
+        ) q
+        ON q.game_session_id = gs.uid
+    GROUP BY gs.user_id
+    ) security
+WHERE security.user_id = auth.uid()
+    OR CURRENT_USER = 'postgres'::name
+    OR CURRENT_USER = 'supabase_admin'::name;
+
